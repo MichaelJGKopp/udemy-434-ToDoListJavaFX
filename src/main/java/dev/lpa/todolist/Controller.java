4937fc5ad65cd83e0fcfe7c3529c29d4713df4da
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-11-15 20:30:22 +0100

    initialize todoItems list

--- src/main/java/dev/lpa/todolist/Controller.java
+++ src/main/java/dev/lpa/todolist/Controller.java
@@ -1,14 +1,39 @@
 package dev.lpa.todolist;
 
+import dev.lpa.todolist.datamodel.TodoItem;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
-  @FXML
-  private Label welcomeText;
 
-  @FXML
-  protected void onHelloButtonClick() {
-    welcomeText.setText("Welcome to JavaFX Application!");
+  private List<TodoItem> todoItems;
+
+  public void initialize() {
+    TodoItem item1 = new TodoItem(
+      "Mail birthday card",
+      "Buy a 30th birthday card for John",
+      LocalDate.of(2024, Month.APRIL, 25));
+    TodoItem item2 = new TodoItem(
+      "Doctor's Appointment",
+      "See Dr. Smith at 123 Main Street. Bring paperwork",
+      LocalDate.of(2024, Month.NOVEMBER, 23));
+    TodoItem item3 = new TodoItem(
+      "Finish design proposal for client",
+      "I promised Mike I'd email website mockups by Friday 22nd April",
+      LocalDate.of(2024, Month.APRIL, 22));
+    TodoItem item4 = new TodoItem(
+      "Pickup Doug at the train station",
+      "Doug's arriving March 23 on the 5:00 train",
+      LocalDate.of(2024, Month.MARCH, 23));
+    TodoItem item5 = new TodoItem(
+      "Pick up dry cleaning",
+      "The clothes should be ready by Wednesday",
+      LocalDate.of(2024, Month.APRIL, 20));
+
+    todoItems = new ArrayList<>(List.of(item1, item2, item3, item4, item5));
   }
 }
--- src/main/java/dev/lpa/todolist/datamodel/TodoItem.java
+++ src/main/java/dev/lpa/todolist/datamodel/TodoItem.java
@@ -2,13 +2,13 @@ package dev.lpa.todolist.datamodel;
 
 import java.time.LocalDate;
 
-public class ToDoItem {
+public class TodoItem {
 
   private String shortDescription;
   private String details;
   private LocalDate deadline;
 
-  public ToDoItem(String shortDescription, String details, LocalDate deadline) {
+  public TodoItem(String shortDescription, String details, LocalDate deadline) {
     this.shortDescription = shortDescription;
     this.details = details;
     this.deadline = deadline;
