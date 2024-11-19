c8388ad6f200564cb9190b0b7b73c1e17b3a42ab
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-11-19 11:13:27 +0100

    change to ObservableList, Data Binding

--- src/main/java/dev/lpa/todolist/Controller.java
+++ src/main/java/dev/lpa/todolist/Controller.java
@@ -68,7 +68,7 @@ public class Controller {
         }
       });
 
-    todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
+    todoListView.setItems(TodoData.getInstance().getTodoItems()); // data binding
     todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
     todoListView.getSelectionModel().selectFirst(); // select first to do list item
   }
@@ -97,11 +97,11 @@ public class Controller {
     if(result.isPresent() && result.get() == ButtonType.OK) {
       DialogController controller = fxmlLoader.getController();
       TodoItem newItem = controller.processResults();  // add item to todoList from dialog fields
-      todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());  // update visual list
+//      todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());  // update visual list
       todoListView.getSelectionModel().select(newItem); // select the just added Item
-      System.out.println("OK pressed");
-    } else {
-      System.out.println("Cancel pressed");
+//      System.out.println("OK pressed");
+//    } else {
+//      System.out.println("Cancel pressed");
     }
   }
 
--- src/main/java/dev/lpa/todolist/datamodel/TodoData.java
+++ src/main/java/dev/lpa/todolist/datamodel/TodoData.java
@@ -1,6 +1,7 @@
 package dev.lpa.todolist.datamodel;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -10,13 +11,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 public class TodoData {
   private static  TodoData instance = new TodoData();
   private static String filename = "TodoListItems.txt";
 
-  private List<TodoItem> todoItems;
+  private ObservableList<TodoItem> todoItems;
   private DateTimeFormatter formatter;
 
   public static TodoData getInstance() {
@@ -27,7 +27,7 @@ public class TodoData {
     formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
   }
 
-  public List<TodoItem> getTodoItems() {
+  public ObservableList<TodoItem> getTodoItems() {
     return todoItems;
   }
 
